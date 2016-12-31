021aa69d8d97d08aa46169bd4bd96273b87849b1
William Shamblin <wjs@cs.duke.edu>
2016-12-31 10:03:45 -0500

    minor update

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # APStatsProject
-# APStatsProject
+
+Ben Shamblin's AP Stats project comparing notable deaths from 2006 through
+2016 in order to determine if 2016 notable deaths were indeed higher than
+normal as some reports have claimed. 
+
+In this comparison, I compared three sources:
+
+Death Server (http://dpsinfo.com/dps/)
+
+Wikipedia Deaths (https://en.wikipedia.org/wiki/YEAR#Deaths)
+
+NY Times Obituaries
+
+The Death Server data was scraped and parsed using a custom script
+(parse_death_stats.py). The Wikipedia data was collected using the
+wikipedia python module (https://pypi.python.org/pypi/wikipedia/) and
+a custom script (wikipedia_search.py) that generates a CSV file from
+the collected data. The NYTimes data
+was collected through their article API (http://developer.nytimes.com/)
+and a custom script to retrieve the results (nytimes.py) and parse
+the returned results into a CSV file (parse_nytimes.py). Using the NYTimes
+API turned out to difficult since you can only return 10 results per page,
+and a maximum of 120 pages from the API. This meant that I had to chop the
+searches into parts and stitch them together. In addition, each API key
+can only be used for 1000 queries per day. After trying to get a higher
+limit from the NYTimes (and failing) I obtained multiple keys and spread
+the queries out over those keys in order to complete the 11 year data
+collection.
+
---
+++ nytimes.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import json
+import re
+import requests
+import time
+
+nyurl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
+# for a full run
+#years = ['2006', '2007', '2008', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016']
+#se_dates = {'01'01':'0331', '0401':'0731', '0801':'12'31'}
+
+# catch up for last couple of days in 2016
+se_dates = {'1130':'1231'}
+years = ['2016']
+
+# read in entire file into string
+api_file = open('.nytimes_api_keys', 'r').read()
+# multiline match for the key
+api_re = re.compile(r'^(?m)api_key = (.*)$')
+key_matches =  api_re.findall(api_file)
+if key_matches:
+    # Find useable key (anyone that is uncommented)
+    api_key = key_matches[0]
+else:
+    raise Exception("No useable key")
+
+for year in years:
+    # Set initial status
+    status = 'OK'
+    # Break up the year into smaller chunks to get around the 120 page search limit
+    for j in se_dates:
+        # Declare a container for the results
+        all_results = []
+        f = open("NYTimesData/" + 'nytimes-'+year+"-"+j+'.json', 'w')
+        # Reset the status to OK for each run, will be overridden if the query fails
+        status = 'OK'
+        # Grad the fields that we are interested in
+        payload['begin_date'] = year + j
+        payload['end_date'] = year + se_dates[j]
+        payload['page'] = 0
+        # iterate over all of the possible pages, nytimes seems to have a limited API
+        for i in range(120):
+            # make the query
+            r = requests.get(nyurl, params=payload)
+            # check return code make sure it is OK (200)
+            if r.status_code == 200 and r.text:
+                # get the returned documents from the json text we were returned
+                docs = json.loads(r.text)['response']['docs']
+                # if we have results, append the to the results container
+                if docs:
+                    all_results.append(docs)
+            # increment the page to get the next page of results
+            payload['page'] = i + 1
+            # this seems to be needed to work around too many queries in a short time
+            time.sleep(3)
+        # write all of the results out to a file
+        f.write(json.dumps(all_results, default=str, indent=2))
+        f.close()
+
